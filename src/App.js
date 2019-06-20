4dd6fecebef410937ca7f09e71865b65106bf644
CarlosHdez <carloseye11@gmail.com>
2019-06-19 22:24:33 -0500

    Add the base inputs for adding a book [WIP]

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import {Navbar, NavItem} from 'react-materialize'
 
+import BookForm from './book_form'
+
 class App extends React.PureComponent {
   render() {
     const brand = (<a>Wan Shi Tong</a>)
     return (
       <div className="app">
-        <Navbar brand={brand}>
-        </Navbar>
+        <Navbar brand={brand} />
+        <BookForm />
       </div>
     )
   }
 }
 
-export default App;
+export default App
