afda1fe7ef2204f1798b09403c058f2fd77400f0
CarlosHdez <carloseye11@gmail.com>
2020-06-07 21:40:21 -0500

    Fix cors

--- site/src/index.js
+++ site/src/index.js
@@ -1,22 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import * as firebase from 'firebase'
 
 import App from 'App'
 import 'stylesheets/index.scss'
 import 'stylesheets/fonts.scss'
-
-// TODO: Move to an .env file
-const firebaseConfig = {
-  apiKey: "AIzaSyDdVOl9QEljM85UWypzJBaZo6wCi35fsFk",
-  authDomain: "wan-shi-tong-9bf7e.firebaseapp.com",
-  databaseURL: "https://wan-shi-tong-9bf7e.firebaseio.com",
-  projectId: "wan-shi-tong-9bf7e",
-  storageBucket: "wan-shi-tong-9bf7e.appspot.com",
-  messagingSenderId: "364668558048",
-  appId: "1:364668558048:web:3349198d2642116e"
-}
-
-// firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(<App />, document.getElementById('root'))
