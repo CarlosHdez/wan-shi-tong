9b43f33630d5e29935bd728b18212fd2889465c5
CarlosHdez <carloseye11@gmail.com>
2020-06-13 00:04:46 -0500

    Add fetch for actual books data

---
+++ site/src/api/books.js
@@ -0,0 +1,8 @@
+const endpoint = `${process.env.REACT_APP_API_HOST}/api/v1/books`
+
+export const listBooks = async () => {
+  const request = await fetch(endpoint)
+  const {data} = await request.json()
+  return data
+}
+
