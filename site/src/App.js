b9e8f0fc1ff132debf799abdd44758fcd1532964
CarlosHdez <carloseye11@gmail.com>
2020-08-31 22:38:37 -0500

    Add router and handle sidebar routes

--- site/src/App.js
+++ site/src/App.js
@@ -1,42 +1,25 @@
 import React, {useState} from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 import Header from 'displays/navigation/header'
 import Sidebar from 'displays/navigation/sidebar'
 import MainWrapper from 'displays/main_wrapper'
-import {SECTIONS} from 'lib/constants'
-
-// const APP_STATE = {
-//   collections: {
-//     books: [],
-//     boardgames: [],
-//     videogames: []
-//   },
-//   display: {
-//     selectedSection: SECTIONS.books,
-//     sidebar: {
-//       expanded: true
-//     }
-//   }
-// }
 
 const App = () => {
-  const [selectedSection, setSelected] = useState(SECTIONS.books)
   const [expanded, setExpanded] = useState(false)
 
   const toggleExpanded = () => setExpanded(!expanded)
 
   return (
-    <div className="app">
-      <Header onIconClick={toggleExpanded} />
-      <div className='main-container'>
-        <Sidebar
-          expanded={expanded}
-          onChangeSelectedSection={setSelected}
-          selectedSection={selectedSection}
-        />
-        <MainWrapper selectedSection={selectedSection} />
+    <Router>
+      <div className="app">
+        <Header onIconClick={toggleExpanded} />
+        <div className='main-container'>
+          <Sidebar expanded={expanded} />
+          <MainWrapper />
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
 
--- site/src/displays/main_wrapper.js
+++ site/src/displays/main_wrapper.js
@@ -1,36 +1,29 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import {
+  Switch,
+  Route
+} from 'react-router-dom'
 
 import BooksWrapper from 'displays/books/wrapper'
-import {SECTIONS} from 'lib/constants'
 
 import 'stylesheets/main_wrapper.scss'
 
-class MainWrapper extends React.PureComponent {
-  static propTypes = {
-    selectedSection: PropTypes.object.isRequired
-  }
-
-  renderContent = () => {
-    switch(this.props.selectedSection) {
-      case SECTIONS.books:
-        return <BooksWrapper />
-      case SECTIONS.videogames:
-        return <div>Vidieo games </div>
-      case SECTIONS.boardgames:
-        return <div>Board games </div>
-      default:
-        return <div> wtf </div>
-    }
-  }
-
-  render() {
-    return (
-      <div className='main-wrapper'>
-        {this.renderContent()}
-      </div>
-    )
-  }
+const MainWrapper = () => {
+  return (
+    <div className='main-wrapper'>
+      <Switch>
+        <Route path='/books'>
+          <BooksWrapper />
+        </Route>
+        <Route path='/videogames'>
+          <div>Video games</div>
+        </Route>
+        <Route path='/boardgames'>
+          <div>board games</div>
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default MainWrapper
